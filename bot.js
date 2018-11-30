9751b6da2f8c57ea5e4ad0b4a2d1fb57bc116c34
RicardoFeras <44910427+RicardoFeras@users.noreply.github.com>
2018-11-30 21:36:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,4 +31,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
  });
 
  
-client.login(process.env.BOT_TOKEN);
+client.login("NTA2NTkzNDA0OTU2ODM1ODcw.DrkZnA.eYzx8LieKycyBRnpF5I2gFdsnP0");
